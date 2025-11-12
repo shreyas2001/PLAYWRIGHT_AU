0bcc9d2253cb4f5c533e7ecea2904f7589f9e50b
shreyas2001 <67593308+shreyas2001@users.noreply.github.com>
2025-11-12 14:04:16 +0530

    Chaging latest code

--- tests/firstprctice.spec.js
+++ tests/firstprctice.spec.js
@@ -2,9 +2,26 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.amazon.com/');
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('cmf');
-  await page.getByRole('button', { name: 'Submit' }).first().click();
+  // await page.pause()
+  const dismisBtn = page.locator('//input[@data-action-type="DISMISS"]');
+  if (await dismisBtn.isVisible()) {
+    await dismisBtn.click();
+  }
+  await page.locator('//input[@id="twotabsearchtextbox"]').click();
+  await page.locator('//input[@id="twotabsearchtextbox"]').fill('cmf headphone');
   await page.getByRole('button', { name: 'Go', exact: true }).click();
-  await page.locator('#a-autoid-1-anounce').click();
+
+// check for dismiss again
+  if (await dismisBtn.isVisible()) {
+    await dismisBtn.click();
+  }
+  await expect(page.getByRole('heading', { name: 'Results', exact: true })).toBeVisible();
+
+  await page.getByRole('link', { name: 'CMF by Nothing Headphone Pro Active Noise Canceling Wireless Headphones Bluetooth 5.4 Over The Ear Headset, 50H ANC Playtime, Bass&Treble Slider, Custom EQ&Spatial Audio, Hi-Res LDAC, Dark Grey', exact: true })
+
+  // await expect(page.getByRole('link', { name: 'Sponsored Ad - CMF by Nothing' })).toContain('5.4');
+  await page.locator('//button[@id="a-autoid-1-announce"]').click();
+
+  // clicking go to cart
+  await expect(page.locator('//a[contains(text(),"Go to Cart")]')).toHaveText('Go to Cart');
 });
