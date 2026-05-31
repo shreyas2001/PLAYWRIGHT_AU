1a43ed127ef815b5c1dd6cd94db56b924f36ed69
shreyas2001 <67593308+shreyas2001@users.noreply.github.com>
2026-05-31 16:26:03 +0530

    Updated API spec file

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -2,25 +2,26 @@ import{test,expect}from'@playwright/test';
 
 test('API test example',async({request})=>{
 
-    const getResponse = await request.get("https://reqres.in/api/users");
+    const getResponse = await request.get("https://jsonplaceholder.typicode.com/users");
     expect(getResponse.status()).toBe(200);
 
     const text = await getResponse.text();
-    expect(text).toContain("George");
+    expect(text).toContain("Leanne Graham");
 
     console.log(text);
 });
 
 test('API POST request example',async({request})=>{
 
-    const respost = await request.post("https://reqres.in/api/users",{
+    const respost = await request.post("https://jsonplaceholder.typicode.com/users",{
         data:{
+            id: 1,
             name: "Shreyas",
-            job: "Automation"
+            username: "Automation"
         }
     })
 
-    expect(Response.status()).toBe(201);
+    expect(respost.status()).toBe(201);
     const responseBody = await respost.json();
-    expect(responseBody).toHaveTest("name","Shreyas")   ;
+    expect(responseBody).toHaveProperty("name", "Shreyas");
 });
